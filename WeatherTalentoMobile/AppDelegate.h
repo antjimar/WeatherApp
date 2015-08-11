ae75e04242fa9a39ab35e349853204ddb20dcb9c
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-08-11 19:55:58 +0200

    Initial Commit

---
+++ WeatherTalentoMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherTalentoMobile
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 11/08/15.
+//  Copyright (c) 2015 TalentoMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
