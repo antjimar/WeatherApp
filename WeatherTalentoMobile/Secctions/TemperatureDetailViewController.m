8ab0ed7c50ab1a49bfbc47efa5aab2803fbb725c
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-08-12 22:29:15 +0200

    Add map

--- WeatherTalentoMobile/Secctions/TemperatureDetailViewController.m
+++ WeatherTalentoMobile/Secctions/TemperatureDetailViewController.m
@@ -9,6 +9,7 @@
 #import "TemperatureDetailViewController.h"
 #import "LocationSelectedEntity.h"
 #import "AverageTemperatureInteractor.h"
+@import MapKit;
 
 @interface TemperatureDetailViewController ()<UIAlertViewDelegate>
 
@@ -17,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *locationNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *temperatureLabel;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *lineWithConstraint;
+@property (weak, nonatomic) IBOutlet MKMapView *locationMap;
 
 @end
 
@@ -56,10 +58,43 @@
                     self.lineWithConstraint.constant = ([averageTemperature floatValue] * 2.5f) + (20.0f * 2.5f);
                     [self.view layoutIfNeeded];
                 } completion:nil];
+                [self setUpMap];
             });
         }
     }];
 }
+- (void)setUpMap {
+    if (self.locationSelectedEntity.locationEntityLatitude == nil || self.locationSelectedEntity.locationEntityLongitude == nil) {
+        [self.locationMap setHidden:YES];
+        
+    } else {
+        [self.locationMap setHidden:NO];
+        CLLocationCoordinate2D locationSearch = CLLocationCoordinate2DMake([self.locationSelectedEntity.locationEntityLatitude doubleValue],
+                                                                            [self.locationSelectedEntity.locationEntityLongitude doubleValue]);
+        MKCoordinateSpan coordinateSpan = MKCoordinateSpanMake(1, 1);
+        MKCoordinateRegion coordinateRegion = MKCoordinateRegionMake(locationSearch, coordinateSpan);
+        [self.locationMap setRegion:coordinateRegion animated:YES];
+        MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+        [annotation setCoordinate:locationSearch];
+        
+        // Get Address
+        CLLocation *location = [[CLLocation alloc] initWithLatitude:[self.locationSelectedEntity.locationEntityLatitude doubleValue]
+                                                          longitude:[self.locationSelectedEntity.locationEntityLongitude doubleValue]];
+        CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+        [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+            if (placemarks.count) {
+                NSDictionary *dictionary = [[placemarks objectAtIndex:0] addressDictionary];
+                [annotation setTitle:dictionary[@"City"]];
+                [annotation setSubtitle:dictionary[@"Country"]];
+            } else {
+                [annotation setTitle:self.locationSelectedEntity.locationEntityName];
+            }
+            [self.locationMap addAnnotation:annotation];
+        }];
+    }
+}
+
+
 - (void)configureView {
     self.locationNameLabel.text = [self.locationSelectedEntity.locationEntityName uppercaseString];
     self.temperatureLabel.text = @"...";
